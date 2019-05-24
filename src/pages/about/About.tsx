40c47d472f109e2e88693d44978d786fb0f7be67
craig-green <greenca6@email.sc.edu>
2019-05-24 12:32:14 -0400

    Link to react

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -5,7 +5,7 @@ const About = () => (
   <>
     <Typography variant="h2" gutterBottom>About</Typography>
     <Typography variant="subtitle1">
-      This application is built using React, and gets it's data/photos from the awesome open API from NASA.
+      This application is built using <a href="https://reactjs.org/" target="_blank">React</a>, and gets it's data/photos from the awesome open API from NASA.
       <br />
       For more information visit <a href="https://api.nasa.gov/api.html#MarsPhotos" target="_blank">NASA's OpenAPI website</a>
     </Typography>
